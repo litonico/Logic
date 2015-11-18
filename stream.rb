85719de3ccf84652ec2fe4df2bd0acc430d62b8a
Lito Nicolai <lito.nicolai@gmail.com>
2015-11-17 20:42:14 -0600

    Ruby streams

---
+++ stream.rb
@@ -0,0 +1,52 @@
+class Stream
+  attr_reader :head
+  def initialize head, &calc
+    @head = head
+    @calc = calc
+  end
+
+  def rest
+    @calc.call
+  end
+
+  def empty?
+    head.nil?
+  end
+
+  def take n
+    s = self
+    res = []
+    n.times do
+      res << s.head
+      s = s.rest
+      unless s.is_a? Stream # If the stream ends before n elems, stop
+        break
+      end
+    end
+    res
+  end
+
+  def append other
+    if self.empty?
+      other
+    else
+      Stream.new(self.head) do
+        if self.rest.nil?
+          other
+        else
+          self.rest.append other
+        end
+      end
+    end
+  end
+
+  def interleave other
+    Stream.new(self.head) do
+      if self.rest.nil?
+        other
+      else
+        other.interleave self.rest.interleave(other)
+      end
+    end
+  end
+end
---
+++ test.rb
@@ -0,0 +1,44 @@
+require 'minitest/autorun'
+require './stream'
+
+class TestStream < Minitest::Test
+  def setup
+    @one_two = Stream.new(1){ Stream.new(2){} }
+
+    def naturals n
+      Stream.new(n) { naturals n+1 }
+    end
+    @nats = naturals 1
+  end
+
+  def test_stream_get_head
+    assert_equal @one_two.head, 1
+  end
+
+  def test_stream_get_rest
+    assert_equal @one_two.rest.head, 2
+    assert_equal 5, @nats.rest.rest.rest.rest.head
+  end
+
+  def test_stream_take_n
+    assert_equal (1..10).to_a, naturals(1).take(10)
+    assert_equal [1,2], @one_two.take(10)
+  end
+
+  def test_stream_append
+    expected = Stream.new(1){ Stream.new(2){ Stream.new(3){} } }
+    assert_equal expected.take(3), @one_two.append(Stream.new(3){}).take(3)
+  end
+
+  def test_stream_interleave
+    expected = Stream.new(1) { 
+      Stream.new("a") { 
+        Stream.new(2) { 
+          Stream.new ("b") {}
+        } 
+      } 
+    }
+    a_b = Stream.new("a") { Stream.new ("b") {} }
+    assert_equal expected.take(4), @one_two.interleave(a_b).take(4)
+  end
+end
