1775c77f00103ef96cd96c395f0847d15bbb50bf
Lito Nicolai <lito.nicolai@gmail.com>
2015-11-18 15:39:43 -0800

    add simple kanren test case

--- src/kanren.rb
+++ src/kanren.rb
@@ -1,7 +1,13 @@
 require 'stream'
 
+class FreshVar; end
+
 class Kanren
-  def self.run_all out, &block
-    []
+  def self.run_all &block
+    question = FreshVar.new
+    block.call(question).nil?
+  end
+
+  def self.unify
   end
 end
--- test/test_kanren.rb
+++ test/test_kanren.rb
@@ -1,10 +1,16 @@
 require 'minitest/autorun'
+require 'minitest/mock'
 require 'kanren'
+
+
+# KanrenVar.new(:q).is KanrenVar(:x) syntax?
 
 class TestKanren < Minitest::Test
   def test_running_kanren_expression
-    assert_equal Kanren.run_all(:q) {}, []
-    assert_equal Kanren.run_all(:q) {
-    }, [FreshVar.new]
+    Kanren.stub(:unify, true) do
+      assert_equal [true], Kanren.run_all{|q| Kanren.unify(q, true) }
+    end
+
+    assert_equal [FreshVar.new], Kanren.run_all {|q| }
   end
 end
